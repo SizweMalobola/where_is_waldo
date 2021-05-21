d338de6b347784b738e1266ab04dc97904000872
SizweMalobola <25reyals@gmail.com>
2021-05-21 18:34:14 +0200

    finished designing Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import styles from "./homeStyle.module.css";
 function Home() {
   return (
-    <div>
-      <h1>this is the home from imaginary kids</h1>
-      <ul>
+    <div
+      style={{
+        background: `url(${process.env.PUBLIC_URL}/assets/home_background.png)`,
+        backgroundAttachment: "fixed",
+      }}
+      className={styles.container}
+    >
+      <div className={styles["header-container"]}>
+        <h1 className={styles["header-title"]}>WHERE IS WALDO!?</h1>
+        <p className={styles["header-intro"]}>
+          {" "}
+          You there! Help us find Waldo. Currently the most dangerous man in the
+          World. Wanted in multiple countries for countless shit. Waldo is
+          extremely deadly, everyone who goes after him ends up dead in a ditch
+          in a matter of hours. Just the action of reading this message has put
+          you in his deadly sights [name]. Now you have no choice but to find
+          him and his crafty accomplices, before you end up dead in a ditch in
+          the middle of nowhere. It's a matter of life and death,{" "}
+          <span className={styles["header-span"]}>FIND WALDO!</span>
+        </p>
+      </div>
+      {/* <ul>
         <Link to="/puzzle/1">
           <li>puzzle 1</li>
         </Link>
@@ -15,7 +34,61 @@ function Home() {
         <Link to="/puzzle/3">
           <li>puzzle 3</li>
         </Link>
-      </ul>
+      </ul> */}
+      <div className={styles["grid-container"]}>
+        <div className={styles["card"]}>
+          <div
+            className={styles["card-top"]}
+            style={{
+              background: `url(${process.env.PUBLIC_URL}/assets/puzzle_1.jpg)`,
+              backgroundSize: "contain",
+            }}
+          ></div>
+          <div className={styles["card-bottom"]}>
+            <button className={styles["card-start"]}>Start</button>{" "}
+            <button className={styles["card-leaderboard"]}>Leaderboard</button>
+          </div>
+        </div>
+        <div className={styles["card"]}>
+          <div
+            className={styles["card-top"]}
+            style={{
+              background: `url(${process.env.PUBLIC_URL}/assets/puzzle_2.jpg)`,
+              backgroundSize: "contain",
+            }}
+          ></div>
+          <div className={styles["card-bottom"]}>
+            <button className={styles["card-start"]}>Start</button>{" "}
+            <button className={styles["card-leaderboard"]}>Leaderboard</button>
+          </div>
+        </div>
+        <div className={styles["card"]}>
+          <div
+            className={styles["card-top"]}
+            style={{
+              background: `url(${process.env.PUBLIC_URL}/assets/puzzle_3.jpeg)`,
+              backgroundSize: "contain",
+            }}
+          ></div>
+          <div className={styles["card-bottom"]}>
+            <button className={styles["card-start"]}>Start</button>{" "}
+            <button className={styles["card-leaderboard"]}>Leaderboard</button>
+          </div>
+        </div>
+        <div className={styles["card"]}>
+          <div
+            className={styles["card-top"]}
+            style={{
+              background: `url(${process.env.PUBLIC_URL}/assets/puzzle_4.jpg)`,
+              backgroundSize: "contain",
+            }}
+          ></div>
+          <div className={styles["card-bottom"]}>
+            <button className={styles["card-start"]}>Start</button>{" "}
+            <button className={styles["card-leaderboard"]}>Leaderboard</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
