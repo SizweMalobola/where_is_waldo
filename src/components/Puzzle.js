973bb33018de44d691a70b655d022662f92325fb
SizweMalobola <25reyals@gmail.com>
2021-05-24 22:27:14 +0200

    added puzzle image

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -1,20 +1,51 @@
 import React from "react";
 import { useHistory } from "react-router";
+import styles from "./puzzleStyle.module.css";
 
 function Puzzle() {
   const history = useHistory();
-  console.log(history);
+  const pathname = history.location.pathname;
+  let puzzleName;
+  let puzzleUrl;
+
+  switch (pathname) {
+    case "/puzzle/1":
+      puzzleName = "puzzle one";
+      puzzleUrl = "/assets/puzzle_1.jpg";
+      break;
+    case "/puzzle/2":
+      puzzleName = "puzzle two";
+      puzzleUrl = "/assets/puzzle_2.jpg";
+      break;
+    case "/puzzle/3":
+      puzzleName = "puzzle three";
+      puzzleUrl = "/assets/puzzle_3.jpeg";
+      break;
+
+    default:
+      puzzleName = "puzzle four";
+      puzzleUrl = "/assets/puzzle_4.jpg";
+      break;
+  }
+
   return (
     <div>
-      <h1>Puzzle here</h1>
-      <button
+      {/* <h1 className={styles["puzzle-title"]}>{puzzleName}</h1> */}
+      <div
+        className={styles["img-container"]}
+        onClick={(e) => {
+          console.log(e.clientX, e.clientY);
+        }}
+      >
+        <img src={process.env.PUBLIC_URL + puzzleUrl} alt="puzzle" />
+      </div>
+      {/* <button
         onClick={() => {
           history.goBack();
         }}
       >
-        {" "}
         back
-      </button>
+      </button> */}
     </div>
   );
 }
