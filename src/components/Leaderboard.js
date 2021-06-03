a9221689c3e30170b57f5f6b30d510d6e6aef1da
SizweMalobola <25reyals@gmail.com>
2021-06-03 09:51:54 +0200

    expanded on leaderboard page

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,21 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./leaderboardStyle.module.css";
 import { useHistory } from "react-router";
+// import { getScore } from "../firebase";
+import { dbRefObject } from "../firebase";
 
 function Leaderboard() {
+  // state
+  const [database, setDatabase] = useState();
+  // global variables
   const history = useHistory();
-  console.log(history);
+  let pathname = history.location.pathname;
+  let lbNum = pathname[pathname.length - 1];
+  // didMount
+  useEffect(() => {
+    const dbRefLeaderboard = dbRefObject.child(lbNum);
+    dbRefLeaderboard.on("value", (snap) => {
+      if (snap.exists()) {
+        setDatabase(Object.values(snap.val()));
+      } else {
+        setDatabase(null);
+      }
+    });
+  }, [lbNum]);
+
   return (
-    <div>
-      <nav>
-        <h1>Leaderboard</h1>
-      </nav>
-      <div></div>
+    <div className={styles["leaderboard-container"]}>
+      <div className={styles["leaderboard-top"]}>
+        <div className={styles["leaderboard-title"]}>
+          <h1>
+            {lbNum === "1"
+              ? "The Gobbling Gluttons"
+              : lbNum === "2"
+              ? "Worldwid Hike"
+              : lbNum === "3"
+              ? "The Unfriendly Gaints"
+              : lbNum === "4"
+              ? "Department Store"
+              : null}
+          </h1>
+          <h2>Leaderboard</h2>
+        </div>
+      </div>
+      <button className={styles["leaderboard-btn"]}>Play</button>
+      <div className={styles["table-container"]}>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
       <button
         onClick={() => {
           history.goBack();
         }}
       >
         Back
+      </button>
+      <button
+        onClick={() => {
+          console.log(database);
+        }}
+      >
+        get score
       </button>
     </div>
   );
