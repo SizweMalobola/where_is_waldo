96b7fe54fb44c9d9d7d6323acb890d5e259c86f0
SizweMalobola <25reyals@gmail.com>
2021-06-05 22:40:00 +0200

    added responsive design to lb page

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import styles from "./leaderboardStyle.module.css";
 import { useHistory } from "react-router";
 import { dbRefObject } from "../firebase";
+import RingLoader from "react-spinners/RingLoader";
 
 function Leaderboard() {
   // state
   const [database, setDatabase] = useState();
+  const [loading, setLoading] = useState(true);
+
   // global variables
   const history = useHistory();
   let pathname = history.location.pathname;
@@ -19,71 +22,79 @@ function Leaderboard() {
       } else {
         setDatabase(null);
       }
+      setLoading(false);
     });
   }, [lbNum]);
-  // Todo render sorted scores from db.
+
   return (
-    <div className={styles["leaderboard-container"]}>
-      <div className={styles["leaderboard-top"]}>
-        <div className={styles["leaderboard-title"]}>
-          <h1>
-            {lbNum === "1"
-              ? "The Gobbling Gluttons"
-              : lbNum === "2"
-              ? "Worldwid Hike"
-              : lbNum === "3"
-              ? "The Unfriendly Gaints"
-              : lbNum === "4"
-              ? "Department Store"
-              : null}
-          </h1>
-          <h2>Leaderboard</h2>
+    <>
+      {loading ? (
+        <RingLoader color={"red"} loading={loading} size={150} />
+      ) : (
+        <div className={styles["leaderboard-container"]}>
+          <div className={styles["leaderboard-top"]}>
+            <div className={styles["leaderboard-title"]}>
+              <h1>
+                {lbNum === "1"
+                  ? "The Gobbling Gluttons"
+                  : lbNum === "2"
+                  ? "Worldwid Hike"
+                  : lbNum === "3"
+                  ? "The Unfriendly Gaints"
+                  : lbNum === "4"
+                  ? "Department Store"
+                  : null}
+              </h1>
+              <h2>Leaderboard</h2>
+            </div>
+          </div>
+          <button
+            className={styles["leaderboard-btn"]}
+            onClick={() => {
+              history.push(`/puzzle/${lbNum}`);
+            }}
+          >
+            Play
+          </button>
+          <div className={styles["table-container"]}>
+            {database ? (
+              <table>
+                <thead>
+                  <tr>
+                    <th className={styles["table-rank"]}>Rank</th>
+                    <th className={styles["table-name"]}>Name</th>
+                    <th className={styles["table-time"]}>Time</th>
+                  </tr>
+                </thead>
+                <tbody className={styles["table-content"]}>
+                  {database
+                    .sort((a, b) => {
+                      return a.time - b.time;
+                    })
+                    .map((obj, index) => {
+                      return (
+                        <tr key={index + obj.time}>
+                          <td>{index + 1}</td>
+                          <td>{obj.name}</td>
+                          <td>{obj.time}s</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            ) : null}
+          </div>
+          <button
+            className={styles["leaderboard-btn"]}
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            Home
+          </button>
         </div>
-      </div>
-      <button
-        className={styles["leaderboard-btn"]}
-        onClick={() => {
-          history.push(`/puzzle/${lbNum}`);
-        }}
-      >
-        Play
-      </button>
-      <div className={styles["table-container"]}>
-        {database ? (
-          <table>
-            <thead>
-              <tr>
-                <th className={styles["table-rank"]}>Rank</th>
-                <th className={styles["table-name"]}>Name</th>
-                <th className={styles["table-time"]}>Time</th>
-              </tr>
-            </thead>
-            <tbody className={styles["table-content"]}>
-              {database
-                .sort((a, b) => {
-                  return a.time - b.time;
-                })
-                .map((obj, index) => {
-                  return (
-                    <tr key={index + obj.time}>
-                      <td>{index + 1}</td>
-                      <td>{obj.name}</td>
-                      <td>{obj.time}s</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        ) : null}
-      </div>
-      <button
-        onClick={() => {
-          history.push("/");
-        }}
-      >
-        Home
-      </button>
-    </div>
+      )}
+    </>
   );
 }
 
